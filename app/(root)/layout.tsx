2d994be83b62bee243b491438d94b7261cd36b59
shivansh22agra <agrawalshivansh22@gmail.com>
2023-08-06 03:21:51 +0530

    leftSideBar update

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -24,9 +24,9 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <NavBar />
-          <main>
+          <main className="flex flex-row">
             <LeftSidebar />
-            <section className="main-container">
+            <section className="flex min-h-screen flex-1 flex-col items-center bg-dark-1 px-6 pb-10 pt-28 max-md:pb-32 sm:px-10">
               <div className="w-full max-w-4xl">{children}</div>
             </section>
             <RightSideBar />
--- components/shared/leftSideBar.tsx
+++ components/shared/leftSideBar.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import sidebarLinks from "@/constants/index.js";
+import Link from "next/link";
+import Image from "next/image";
 
-const LeftSidebar = () => {
+
+const leftSideBar = () => {
   return (
-    <div>LeftSidebar</div>
-  )
-}
+    <div className="custom-scrollbar sticky left-0 top-0 z-20 flex h-screen w-fit flex-col justify-between overflow-auto border-r border-r-dark-4 bg-dark-2 pb-5 pt-28 max-md:hidden">
+      <div className="flex  flex-1 flex-col gap-6 px-6 ">
+        {sidebarLinks.map((links) => {
+          return (
+            <Link
+              href={links.route}
+              key={links.route}
+              className="relative flex justify-start gap-4 rounded-lg p-4  text-white"
+            >
+              <Image src={links.imgURL} width={30} height={30} alt="" />
+              <p className="text-light-1 max-lg:hidden"> {links.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default LeftSidebar
+export default leftSideBar;
