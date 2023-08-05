bd7a49bfba97bc79765ed9a2f403f0f2f6e5d811
shivansh22agra <agrawalshivansh22@gmail.com>
2023-08-06 00:25:26 +0530

    layout.tsx folder update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,10 +4,11 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
+      
 
 
 
-      <UserButton afterSignOutUrl="/"/>
+      {/* <UserButton afterSignOutUrl="/"/> */}
     </div>
   )
 }
