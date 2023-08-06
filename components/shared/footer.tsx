5e9d547fd5fa3c6a839f6544ad418495f07104a6
shivansh22agra <agrawalshivansh22@gmail.com>
2023-08-06 23:08:59 +0530

    Bottombar update

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import sidebarLinks from "@/constants/index.js";
+import Link from "next/link";
+import Image from "next/image";
+
+import { usePathname, useRouter } from "next/navigation";
 
 const Footer = () => {
+  const router = useRouter();
+  const pathname = usePathname();
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="fixed bottom-0 z-10 w-full rounded-t-3xl bg-glassmorphism p-4 backdrop-blur-sm  md:hidden">
+      <section className=" flex flex-row  justify-between items-center ">
+        {sidebarLinks.map((links) => {
+          const isActive = pathname === links.route;
+          console.log(`___pathname ${pathname} ${links.route} ${isActive}`);
 
-export default Footer
+          return (
+            <Link
+              href={links.route}
+              key={links.route}
+              className={` ${
+                isActive && "bg-primary-500"
+              }    rounded-lg p-2  text-white`}
+            >
+              <Image src={links.imgURL} width={14} height={14} alt="" />
+            </Link>
+          );
+        })}
+      </section>
+    </div>
+  );
+};
+
+export default Footer;
