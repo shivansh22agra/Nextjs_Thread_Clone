ae188ba1bfe331e12930a00bb367e923d96766ba
shivansh22agra <agrawalshivansh22@gmail.com>
2023-08-14 00:00:20 +0530

    ThreadnCard  update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,45 @@
 //app/page.tsx
+import ThreadCard from "@/components/ThreadCard";
 import { fetchThread } from "@/lib/actions/threads.action";
-import { UserButton } from "@clerk/nextjs";
+import User from "@/lib/models/userModel";
+import { UserButton, currentUser } from "@clerk/nextjs";
 export default async function Home() {
   const result = await fetchThread(1, 30);
+  const user = await currentUser();
   console.log(`cosomidfmf ${result?.posts}`);
 
   return (
     <div className="text-left head-text">
       Home
       <section className="mt-9 flex flex-col gap-10">
-       {result?.posts.map((posts)=>{
-        return <div></div>
-       })}</section>{" "}
+        {result?.posts.length === 0 ? (
+          <p>No Threads found</p>
+        ) : (
+          <>
+            {result?.posts.map((post) => {
+              return (
+                <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+                />
+              );
+            })}
+            
+      {/* <Pagination
+        path='/'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      /> */}
+          </>
+        )}
+      </section>
     </div>
   );
 }
