57c9504cc3adc850ae337a1fec4794dfc958b050
shivansh22agra <agrawalshivansh22@gmail.com>
2023-08-15 16:38:59 +0530

    Thread Card ui update

--- components/ThreadCard.tsx
+++ components/ThreadCard.tsx
@@ -1,3 +1,6 @@
+import { auth } from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 interface Props {
   id: string;
@@ -34,7 +37,69 @@ function ThreadCard({
   comments,
   isComment,
 }: Props) {
-  return <div>ThreadCard</div>;
+  return (
+    <section className="flex flex-col w-full rounded-xl bg-dark-2 p-7">
+      <div className=" flex items-start justify-between">
+        <div className="flex w-full flex-1 flex-row gap-4">
+          <div className="flex flex-col items-center">
+            <Link
+              href={`/profile/${author.id}`}
+              className=" relative h-11 w-11"
+            >
+              <Image
+                src={author.image}
+                alt="Profile image"
+                fill
+                className="rounded-full cursor-pointer"
+              />
+            </Link>
+            {/* <div className="relative mt-2 w-0.5 grow rounded-full bg-neutral-800" /> */}
+          </div>
+          <div className="flex w-full flex-col">
+            <Link
+              href={`/profile/${author.id}`}
+              className=" w-fit cursor-pointer"
+            >
+              {author.name}
+            </Link>
+            <p className="mt-2 text-light-1 text-small-regular">{content}</p>
+            <div className="flex flex-col mt-5 gap-3">
+              <div className="flex gap-3.5">
+                <Image
+                  src="/assets/heart-gray.svg"
+                  alt="heart"
+                  width={22}
+                  height={22}
+                  className="cursor-pointer"
+                />
+                <Image
+                  src="/assets/reply.svg"
+                  alt="reply"
+                  width={22}
+                  height={22}
+                  className="cursor-pointer"
+                />{" "}
+                <Image
+                  src="/assets/repost.svg"
+                  alt="repost"
+                  width={22}
+                  height={22}
+                  className="cursor-pointer"
+                />
+                <Image
+                  src="/assets/share.svg"
+                  alt="share"
+                  width={22}
+                  height={22}
+                  className="cursor-pointer"
+                />{" "}
+              </div>
+            </div>{" "}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default ThreadCard;
